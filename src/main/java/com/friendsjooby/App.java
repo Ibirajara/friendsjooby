48030b93296d28c6842ac0406453ba9e3ee04127
Ibirajara Barrel Junior <ibirajara.junior@gmail.com>
2016-05-12 18:12:14 -0300

    Segundo commit.

--- src/main/java/com/friendsjooby/App.java
+++ src/main/java/com/friendsjooby/App.java
@@ -1,18 +1,84 @@
 package com.friendsjooby;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
 import org.jooby.Jooby;
+import org.jooby.MediaType;
+import org.jooby.Results;
+import org.jooby.json.Jackson;
 
 /**
  * @author jooby generator
  */
 public class App extends Jooby {
-
+    ArrayList<Contato> contatos = new ArrayList<>();
+    Integer nc;
+    ObjectMapper mapper = new ObjectMapper();
   {
-    get("/", () -> "Hello World!");
+      use(new Jackson() );
+    get("/todos/:id", (req) -> {
+        Integer id = null;
+        String result = null;
+        int statusCode = 404;
+        id = Integer.parseInt(req.param("id").value());
+        for(Contato c: contatos){
+            if(c.getId() == id){
+                result = mapper.writeValueAsString(c);
+            }
+        }
+        if(result != null){
+            return Results.with(result).status(200).type(MediaType.text);
+        }else{
+            return Results.with("Contact not found!").status(statusCode).type(MediaType.text);
+        }
+        
+    });
+    
+    get("/todos/listall", req -> {
+            return contatos;
+        }).name("list all contacts.");
+    
+    post("/todos", req -> {
+        String message = null;
+        String jsonInString = req.body().value();
+        Contato c = mapper.readValue(jsonInString, Contato.class);
+        c.setId(newFriend());
+        message = mapper.writeValueAsString(c);
+        int statusCode = 400;
+            if (c.getName().equals("")) {
+                message = "Name cannot be empty!";
+            } else if (c.getPhone().isEmpty()) {
+                message = "Phone cannot be empty!";
+            } else {
+                statusCode = 200;
+                c.setId(this.contatos.size()+1);
+                this.contatos.add(c);
+            }
+
+            return Results.with(message).status(statusCode).type("text/plain");
+        }
+
+    )
+    .consumes(MediaType.json)
+    .name("Insert a contact");
+
+    
+
   }
+  
 
   public static void main(final String[] args) throws Throwable {
     run(App::new, args);
   }
-
+  
+  private Integer newFriend(){
+      nc = 0;
+      for(Contato c: contatos){
+          if(c.getId() > nc){
+              nc = c.getId();
+          }
+      }
+      return nc += 1;
+  }
+  
 }
---
+++ src/main/java/com/friendsjooby/Contato.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 ibirajara.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.friendsjooby;
+
+/**
+ *
+ * @author ibirajara
+ */
+public class Contato {
+    private Integer id;
+    private String name;
+    private String phone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    
+}
