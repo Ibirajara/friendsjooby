6feb537e515df748941cd7b7dfb1086234aa79ca
Ibirajara Barrel Junior <ibirajara.junior@gmail.com>
2016-05-15 01:08:30 -0300

    Segundo commit.

--- src/main/java/com/friendsjooby/App.java
+++ src/main/java/com/friendsjooby/App.java
@@ -8,15 +8,31 @@ import org.jooby.Results;
 import org.jooby.json.Jackson;
 
 /**
- * @author jooby generator
+ * @author Ibirajara Barrel Junior
+ * Sistema de Agenta com inserção e busca de contato.
  */
 public class App extends Jooby {
     ArrayList<Contato> contatos = new ArrayList<>();
     Integer nc;
     ObjectMapper mapper = new ObjectMapper();
+  
+ 
   {
       use(new Jackson() );
-    get("/todos/:id", (req) -> {
+    /**
+     * @author Ibirajara Barrel Junior
+     * Página de boas vindas.
+     */
+    get("/", () -> "Welcome to your Address book!!");
+      
+    /**
+     * @author Ibirajara Barrel Junior
+     * Página de busca de contato por id.
+     * O id do usuario é passado como parâmetro.
+     * Deve retornar o contato se encontrado, ou uma mensagem de não encontrado
+     * quando for o caso.
+     */
+    get("/find:id", (req) -> {
         Integer id = null;
         String result = null;
         int statusCode = 404;
@@ -32,12 +48,19 @@ public class App extends Jooby {
             return Results.with("Contact not found!").status(statusCode).type(MediaType.text);
         }
         
-    });
-    
+    }).name("Find a contact by id.");
+    /**
+     * @author Ibirajara Barrel Junior
+     * Retorna todos os contatos da lista.
+     */
     get("/todos", req -> {
             return contatos;
         }).name("list all contacts.");
     
+    /**
+     * @author Insere um contato. faz a verificaçao dos campos name e phone
+     * e conclui a insersao, retornando um json do contato inserido.
+     */
     post("/todos", req -> {
         String message = null;
         String jsonInString = req.body().value();
@@ -70,7 +93,10 @@ public class App extends Jooby {
   public static void main(final String[] args) throws Throwable {
     run(App::new, args);
   }
-  
+  /**
+   * Retorna o novo codigo do usuario.
+   * @return id do novo usuario
+   */
   private Integer newFriend(){
       nc = 0;
       for(Contato c: contatos){
--- src/test/java/com/friendsjooby/AppTest.java
+++ src/test/java/com/friendsjooby/AppTest.java
@@ -1,17 +1,32 @@
 package com.friendsjooby;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * @author jooby generator
  */
 public class AppTest extends BaseTest {
+    static String CONTACT_IN = "{\"name\":\"Usuário de Teste\","
+            +" \"phone\":\"99991234\"}";
+    static String CONTAC_OUT = "{\"id\":1,"
+            + "\"name\":\"Usuário de Teste\","
+            +"\"phone\":\"99991234\"}";
+    @Test
+    public void insert() throws Exception {
+        String json = "{\n" +
+            "    \"name\":\"Usuário de Teste\",\n" +
+            "    \"phone\":\"99991234\"\n" +
+        "}";
 
-  @Test
-  public void index() throws Exception {
-    server.get("/")
-        .expect(200)
-        .header("Content-Type", "text/html;charset=UTF-8");
-  }
+        server.post("/todos")
+            .body(json, "application/json")
+            .expect(200);
+    }
+    @Test
+    public void findById() throws Exception{
+        server.post("/todos").body(CONTACT_IN, "application/json");
+        server.get("/find1").expect(CONTAC_OUT).expect(200);
+    }
 
 }
